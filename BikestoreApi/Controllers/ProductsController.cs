5440cc901e84229ee8e4eae2b74ac696f4e42faa
Mateusz Sałata <m.salata.123@studms.ug.edu.pl>
2021-06-03 19:37:28 +0200

    Add project files.

---
+++ BikestoreApi/Controllers/ProductsController.cs
@@ -0,0 +1,69 @@
+﻿using BikestoreApi.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BikestoreApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ProductContext _context;
+
+        public ProductsController(ProductContext context)
+        {
+            _context = context;
+
+            if (_context.Products.Count() == 0)
+            {
+                _context.Products.Add(new Product { name = "Rower 1" });
+                _context.SaveChanges();
+            }
+        }
+        [HttpGet]
+        public IEnumerable<Product> GetProducts()
+        {
+            return _context.Products.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Product> GetProduct(int id)
+        {
+            var Product = _context.Products.Find(id);
+
+            if (Product == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Product;
+            }
+        }
+        [HttpPost]
+        public ActionResult<Product> PostProduct(Product product)
+        {
+            _context.Products.Add(product);
+            _context.SaveChanges();
+
+            return CreatedAtAction(nameof(GetProduct), new { id = product.id }, product);
+        }
+        [HttpPut("{id}")]
+        public ActionResult<Product> PutProduct(int id, Product product)
+        {
+            if (id != product.id)
+            {
+                return BadRequest();
+            }
+
+                _context.Products.Update(product);
+                _context.SaveChanges();
+
+            return NoContent();
+        }
+    }
+}
---
+++ BikestoreApi/Model/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BikestoreApi.Model
+{
+    public class Product
+    {
+        public int id { get; set; }
+        public string type { get; set; }
+        public string name { get; set; }
+        public string manufacturer { get; set; }
+        public int price { get; set; }
+        public int availability { get; set; }
+    }
+}
